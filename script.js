ba13dd37f9a00f50f52edefbe15032fdd07f77d1
Dennis Amparo <dennisamparo11@gmail.com>
2020-06-18 19:15:14 -0700

    added more shapes with timer

--- script.js
+++ script.js
@@ -1,35 +1,70 @@
 var redsquare = document.querySelector(".square");
-
-
 var wholecross = document.querySelector(".cross");
+var toprightTri = document.querySelector(".triangle-topright");
+var botleftTri = document.querySelector(".triangle-bottomleft");
+var circle = document.querySelector(".circle");
 
-var timer;
 
-var count = 1
+// var timer;
+
+var count = 1;
 
 redsquare.addEventListener("click", function () {
+  redsquare.setAttribute("style", "display: none");
 
-var timer = setInterval(function(){
+  wholecross.setAttribute("style", "display: block");
+
+  var timer = setInterval(function () {
     console.log(count++);
-            
-    }, 1000);
+  }, 1000);
 
-
-setTimeout(function() {
+  setTimeout(function () {
     clearInterval(timer);
-    console.log("its working, its working");
-        
+    // console.log("its working, its working");
+
+    wholecross.setAttribute("style", "display: none");
+
+    toprightTri.setAttribute("style", "display:block");
+
     
-    redsquare.setAttribute("style", "display: none");
+
+  }, 2000);
+
+
+  var timer2 = setInterval(function () {
+    console.log(count++);
+  }, 1000);
+
+  setTimeout(function () {
+    clearInterval(timer2);
+
+    toprightTri.setAttribute("style", "display:none");
+
+    botleftTri.setAttribute("style", "display:block");
+
+
     
+  }, 4000);
+
+  var timer3 = setInterval(function () {
+    console.log(count++);
+  }, 1000);
+
+  setTimeout(function () {
+    clearInterval(timer3);
+
+
+
+    botleftTri.setAttribute("style", "display:none");
+    circle.setAttribute("style", "display:block");
+
+
     
-    wholecross.setAttribute("style", "display: block");
-    
-    
-    
-}, 5000);
+  }, 6000);
+
+
+
+
 
 
 });
-
-
